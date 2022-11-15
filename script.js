83795e0bf6a502c971e0367c3cc7252de7ac42e7
Marina Zinchenko <iolsoofuolso@gmail.com>
2022-11-15 13:58:45 -0800

    init

---
+++ script.js
@@ -0,0 +1,14 @@
+'use strict';
+ 
+document.getElementById("bored-bot").addEventListener("click", getIdea)
+
+function getIdea() {
+    fetch("https://www.boredapi.com/api/activity")
+        .then(res => res.json())
+        .then(data => {
+            document.body.classList.add("fun")
+            document.getElementById("idea").textContent = data.activity
+            document.getElementById("title").textContent = "🦾 HappyBot🦿"
+        })
+}
+
